eb4f916fb3ed90ada959a228decdc822b67a9d47
upwon <1244753906@qq.com>
2021-09-10 22:30:45 +0800

    工具类添加注释

--- app/src/main/java/com/example/myandroidnotes/util/MyTimeUtil.java
+++ app/src/main/java/com/example/myandroidnotes/util/MyTimeUtil.java
@@ -1,12 +1,33 @@
 package com.example.myandroidnotes.util;
 
+import android.annotation.SuppressLint;
+
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+/**
+ * @ProjectName:
+ * @Package: com.example.myandroidnotes.util
+ * @ClassName: MyTimeUtil
+ * @Description: 与时间相关的操作类
+ * @Author: wangxianwen
+ * @CreateDate: 2021/9/10 22:25
+ * @UpdateUser: 更新者
+ * @UpdateDate: 2021/9/10 22:25
+ * @UpdateRemark: 更新内容
+ * @Version: 1.0
+ */
 public class MyTimeUtil {
+
+    /**
+     * @method  getCurrentTime
+     * @description 获取当前时间 格式为 yyyy年MM月dd日 HH:mm:ss
+     * @date: 2021/9/10 22:25
+     * @author: wangxianwen
+     */
     public static String getCurrentTime() {
-        SimpleDateFormat sdf = new SimpleDateFormat("yyyy年MM月dd日 HH:mm:ss");
-        Date date=new Date();
+        @SuppressLint("SimpleDateFormat") SimpleDateFormat sdf = new SimpleDateFormat("yyyy年MM月dd日 HH:mm:ss");
+        Date date = new Date();
         return sdf.format(date);
 
     }
--- app/src/main/java/com/example/myandroidnotes/util/SpfUtil.java
+++ app/src/main/java/com/example/myandroidnotes/util/SpfUtil.java
@@ -3,9 +3,29 @@ package com.example.myandroidnotes.util;
 import android.content.Context;
 import android.content.SharedPreferences;
 
+/**
+ * @ProjectName: SharedPreferences 工具类
+ * @Package: com.example.myandroidnotes.util
+ * @ClassName:
+ * @Description: java类作用描述
+ * @Author: wangxianwen
+ * @CreateDate: 2021/9/10 22:27
+ * @UpdateUser: 更新者
+ * @UpdateDate: 2021/9/10 22:27
+ * @UpdateRemark: 更新内容
+ * @Version: 1.0
+ */
 public class SpfUtil {
     private static String SPF_NAME = "noteSpf";
 
+    /**
+     * @method  saveString
+     * @description 存储键值对至SharedPreferences
+     * @date: 2021/9/10 22:28
+     * @author: wangxianwen
+     * @param
+     * @return
+     */
     public static void saveString(Context context, String key, String value) {
         SharedPreferences spf = context.getSharedPreferences(SPF_NAME, Context.MODE_PRIVATE);
         SharedPreferences.Editor edit = spf.edit();
@@ -13,6 +33,14 @@ public class SpfUtil {
         edit.apply();
     }
 
+    /**
+     * @method
+     * @description 从SharedPreferences获取 键值对的value
+     * @date: 2021/9/10 22:29
+     * @author: wangxianwen
+     * @param
+     * @return
+     */
     public static String getString(Context context, String key) {
         SharedPreferences spf = context.getSharedPreferences(SPF_NAME, Context.MODE_PRIVATE);
         return spf.getString(key, "");
