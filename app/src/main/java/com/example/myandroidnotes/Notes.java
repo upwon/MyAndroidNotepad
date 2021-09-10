7faf1023358cb9c34e9e2f22db1c365b616b5803
upwon <1244753906@qq.com>
2021-09-10 20:20:01 +0800

    Update Notes.java

--- app/src/main/java/com/example/myandroidnotes/Notes.java
+++ app/src/main/java/com/example/myandroidnotes/Notes.java
@@ -7,77 +7,69 @@ import org.litepal.crud.LitePalSupport;
 
 import java.io.Serializable;
 
-public class Notes extends LitePalSupport implements Serializable  {
+public class Notes extends LitePalSupport implements Serializable {
 
 
-
-        private long id;
-//        private Integer item_imageID;
-        private String title;
-        private String contents;
-        private String time;
+    private long id;
+    private String title;
+    private String contents;
+    private String time;
 
     public Notes() {
 
     }
 
     public Notes(String title, String contents, String time) {
-         //   this.item_imageID = item_imageID;
-            this.title = title;
-            this.contents = contents;
-            this.time = time;
-        }
+        //   this.item_imageID = item_imageID;
+        this.title = title;
+        this.contents = contents;
+        this.time = time;
+    }
 
 
-        public long getId() {
-            return id;
-        }
+    public long getId() {
+        return id;
+    }
 
-        public void setId(long id) {
-            this.id = id;
-        }
+    public void setId(long id) {
+        this.id = id;
+    }
 
-//        public Integer getItem_imageID() {
-//            return item_imageID;
-//        }
-//
-//        public void setItem_imageID(Integer item_imageID) {
-//            this.item_imageID = item_imageID;
-//        }
 
-        public String getTitle() {
-            return title;
-        }
 
-        public void setTitle(String title) {
-            this.title = title;
-        }
+    public String getTitle() {
+        return title;
+    }
 
-        public String getContents() {
-            return contents;
-        }
+    public void setTitle(String title) {
+        this.title = title;
+    }
 
-        public void setContents(String contents) {
-            this.contents = contents;
-        }
+    public String getContents() {
+        return contents;
+    }
 
-        public String getTime() {
-            return time;
-        }
+    public void setContents(String contents) {
+        this.contents = contents;
+    }
 
-        public void setTime(String time) {
-            this.time = time;
-        }
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
 
 
     @NonNull
     @Override
     public String toString() {
-        return "Note{"+
-                "title='"+title+'\''+
-                ", content='"+contents+'\''+
-                ", createdTime='"+time+'\''+
-                ", id="+id+'\''+
+        return "Note{" +
+                "title='" + title + '\'' +
+                ", content='" + contents + '\'' +
+                ", createdTime='" + time + '\'' +
+                ", id=" + id + '\'' +
                 '}';
     }
 }
