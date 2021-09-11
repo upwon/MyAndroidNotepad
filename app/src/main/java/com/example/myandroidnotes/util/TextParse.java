e8b29807946a1c4a928fd994471ad5b6956f6e67
upwon <1244753906@qq.com>
2021-09-11 10:10:28 +0800

    更新正则表达式

--- app/src/main/java/com/example/myandroidnotes/util/TextParse.java
+++ app/src/main/java/com/example/myandroidnotes/util/TextParse.java
@@ -1,6 +1,8 @@
 package com.example.myandroidnotes.util;
 
 
+import android.util.Log;
+
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 
@@ -16,49 +18,51 @@ import java.util.regex.Pattern;
 
 public class TextParse {
 
+    private static final String TAG = "TextParse";
+
     /**
+     * @param
+     * @return
      * @method getPlainTextFromHTML
      * @description 从HTML提取出出文本 改方法只能简单地去掉空格，制表符，回车，换行
      * @date: 2021/9/10 20:01
      * @author: wangxianwen
-     * @param
-     * @return
      */
-    public static String  getPlainTextFromHTML(String inputHTML){
+    public static String getPlainTextFromHTML(String inputHTML) {
 
-        String regMatchEnter="\\s*|\t|\r|\n";
+        String regMatchEnter = "\\s*|\t|\r|\n";
 
         Pattern p = Pattern.compile(regMatchEnter);
         Matcher m = p.matcher(inputHTML);
-        String outputText=m.replaceAll("");
+        String outputText = m.replaceAll("");
 
         return outputText;
     }
 
     /**
-     * @method  parsePlainTextFromHTML
+     * @param
+     * @return
+     * @method parsePlainTextFromHTML
      * @description 从HTML解析文本  使用Jsoup库
      * @date: 2021/9/10 22:31
      * @author: wangxianwen
-     * @param
-     * @return
      */
-    public static String  parsePlainTextFromHTML(String inputHTML) {
+    public static String parsePlainTextFromHTML(String inputHTML) {
         Document d = Jsoup.parse(inputHTML);
         String text = d.text();
         return text;
     }
 
     /**
-     * @method  Html2Text
+     * @param inputString 含html标签的字符串
+     * @return
+     * @method Html2Text
      * @description 从html中提取纯文本
      * @date: 2021/9/10 20:02
      * @author: wangxianwen
-     * @param
-     * @return
      */
     public static String Html2Text(String inputString) {
-        String htmlStr = inputString; // 含html标签的字符串
+        String htmlStr = inputString;
         String textStr = "";
         java.util.regex.Pattern p_script;
         java.util.regex.Matcher m_script;
@@ -67,24 +71,37 @@ public class TextParse {
         java.util.regex.Pattern p_html;
         java.util.regex.Matcher m_html;
         try {
-            String regEx_script = "<[\\s]*?script[^>]*?>[\\s\\S]*?<[\\s]*?\\/[\\s]*?script[\\s]*?>"; // 定义script的正则表达式{或<script[^>]*?>[\\s\\S]*?<\\/script>
-            String regEx_style = "<[\\s]*?style[^>]*?>[\\s\\S]*?<[\\s]*?\\/[\\s]*?style[\\s]*?>"; // 定义style的正则表达式{或<style[^>]*?>[\\s\\S]*?<\\/style>
-            String regEx_html = "<[^>]+>"; // 定义HTML标签的正则表达式
+            // 定义script的正则表达式{或<script[^>]*?>[\\s\\S]*?<\\/script>
+            String regEx_script = "<[\\s]*?script[^>]*?>[\\s\\S]*?<[\\s]*?\\/[\\s]*?script[\\s]*?>";
+            // 定义style的正则表达式{或<style[^>]*?>[\\s\\S]*?<\\/style>
+            String regEx_style = "<[\\s]*?style[^>]*?>[\\s\\S]*?<[\\s]*?\\/[\\s]*?style[\\s]*?>";
+            // 定义HTML标签的正则表达式
+            String regEx_html = "<[^>]+>";
             p_script = Pattern.compile(regEx_script, Pattern.CASE_INSENSITIVE);
             m_script = p_script.matcher(htmlStr);
-            htmlStr = m_script.replaceAll(""); // 过滤script标签
+
+            // 过滤script标签
+            htmlStr = m_script.replaceAll("");
             p_style = Pattern.compile(regEx_style, Pattern.CASE_INSENSITIVE);
             m_style = p_style.matcher(htmlStr);
-            htmlStr = m_style.replaceAll(""); // 过滤style标签
+
+            // 过滤style标签
+            htmlStr = m_style.replaceAll("");
             p_html = Pattern.compile(regEx_html, Pattern.CASE_INSENSITIVE);
             m_html = p_html.matcher(htmlStr);
-            htmlStr = m_html.replaceAll(""); // 过滤html标签
+
+            // 过滤html标签
+            htmlStr = m_html.replaceAll("");
             textStr = htmlStr;
-        } catch (Exception e) {System.err.println("Html2Text: " + e.getMessage()); }
+        } catch (Exception e) {
+            Log.d(TAG, "Html2Text: " + e.getMessage());
+        }
         //剔除空格行
-        textStr=textStr.replaceAll("[ ]+", " ");
-        textStr=textStr.replaceAll("(?m)^\\s*$(\\n|\\r\\n)", "");
-        return textStr;// 返回文本字符串
+        textStr = textStr.replaceAll("[ ]+", " ");
+        textStr = textStr.replaceAll("(?m)^\\s*$(\\n|\\r\\n)", "");
+
+        // 返回文本字符串
+        return textStr;
     }
 
 }
