641c1b6c68ebfd53bf8f3659bba691586fe1ca2b
upwon <1244753906@qq.com>
2021-09-26 21:11:05 +0800

    Create GlideEngine.java

---
+++ app/src/main/java/com/example/myandroidnotes/GlideEngine.java
@@ -0,0 +1,12 @@
+package com.example.myandroidnotes;
+
+
+/**
+ * @ClassName: GlideEngine
+ * @Description: java类作用描述
+ * @Author: wangxianwen
+ * @Date: 2021/9/26 21:10
+ */
+
+public class GlideEngine {
+}
